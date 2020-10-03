7cffcadab3e725ec6f3c7f301997270021093592
Shruti Hazra <69050282+shrutihazra1998@users.noreply.github.com>
2020-10-03 16:08:07 +0530

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamWebApp.Models
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string EmpAddress { get; set; }
+        public double EmpSalary { get; set; }
+        public override string ToString()
+        {
+            return $"<p>Employee Id: {EmpID}</P><p> Employee Name: {EmpName}</p><p>Employee Address: {EmpAddress}</p><p>Employee Salary: {EmpSalary}</p>";
+        }
+    }
+}
